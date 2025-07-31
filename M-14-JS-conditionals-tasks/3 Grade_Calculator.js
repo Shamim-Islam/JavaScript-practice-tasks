85245f5f2d0899a205e591240286264ea3dd19f0
Shamim-Islam <shamimislam844gm@gmail.com>
2025-07-31 16:42:58 +0600

    code updated

--- M-14-JS-conditionals-tasks/3 Grade_Calculator.js
+++ M-14-JS-conditionals-tasks/3 Grade_Calculator.js
@@ -11,3 +11,26 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
+
+// Grade Calculator
+
+//Take input from users
+//let score = prompt("Enter the students score 0-100: "); // wont be working in console environment
+
+// For testing purposes, we can set a score directly
+let score = 49;
+
+//convert score to a number
+score = Number(score);
+
+if (score >= 90 && score <= 100) {
+  console.log("Your grade is A");
+} else if (score >= 80 && score < 90) {
+  console.log("Your grade is B");
+} else if (score >= 70 && score < 80) {
+  console.log("Your grade is C");
+} else if (score >= 60 && score < 70) {
+  console.log("Your grade is D");
+} else {
+  console.log("Your grade is F");
+}
