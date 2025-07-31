b31a8b7af086939bb793a66bc0f9f4ed3847d13e
Shamim-Islam <shamimislam844gm@gmail.com>
2025-07-31 17:58:36 +0600

    code updated

--- M-15_Js_loops_tasks/while/3 sum of odd numbers.js
+++ M-15_Js_loops_tasks/while/3 sum of odd numbers.js
@@ -1,5 +1,3 @@
-
-
 /***
 
 Subtask-1:
@@ -7,6 +5,19 @@ Subtask-1:
 Display sum of all the odd numbers from 81 to 131.
 
  */
+
+let num1 = 81;
+let sumOdd = 0;
+
+while (num1 <= 131) {
+  if (num1 % 2 !== 0) {
+    sumOdd += num1;
+  }
+  num1++;
+}
+
+console.log("Sum of odd numbers from 81 to 131 is: " + sumOdd);
+
 /***
 
 Subtask-2:
@@ -16,3 +27,15 @@ Display sum of all the even numbers from 206 to 311.
  */
 
 /*programming hero*/
+
+let num2 = 206;
+let sumEven = 0;
+
+while (num2 <= 311) {
+  if (num2 % 2 === 0) {
+    sumEven += num2;
+  }
+  num2++;
+}
+
+console.log("Sum of even numbers from 206 to 311 is: " + sumEven);
