36eb6882e7bf1332d304f1fbf8cabba62f1b664e
Shamim-Islam <shamimislam844gm@gmail.com>
2025-07-31 18:04:26 +0600

    code updated

--- M-15_Js_loops_tasks/while/4 multiplication table.js
+++ M-15_Js_loops_tasks/while/4 multiplication table.js
@@ -4,5 +4,13 @@ As Ersa is learning now, she wants to explore more and more. Tell Ersa to genera
 
  */
 
-
 /*programming hero*/
+
+// Multiplication table for number 5
+
+let number = 1;
+
+while (number <= 10) {
+  console.log(`5 x ${number} = ${5 * number}`);
+  number++;
+}
--- M-15_Js_loops_tasks/while/5 countdown.js
+++ M-15_Js_loops_tasks/while/5 countdown.js
@@ -4,5 +4,11 @@ Implement a countdown timer that counts down from 21 to 15.
 
  */
 
-
 /*programming hero*/
+
+let countdown = 21;
+
+while (countdown >= 15) {
+  console.log(countdown);
+  countdown--;
+}
