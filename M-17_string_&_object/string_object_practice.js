942cdf9ae86395b26bbb0ff819e83ae3ec4b5934
Shamim-Islam <shamimislam844gm@gmail.com>
2025-08-02 17:45:57 +0600

    code updated

---
+++ M-17_string_&_object/string_object_practice.js
@@ -0,0 +1,90 @@
+const country = "Bangladesh";
+const division = "Dhaka";
+const district = `nowkhali`;
+const thana = new String("Ashulia");
+
+// console.log(thana);
+
+// length of string
+// console.log(country.length); // string
+
+// index number of string
+// console.log(country[1]);
+
+// const school = "RAJ UK Uttara Model School";
+// const college = "bepza college";
+
+// console.log(school);
+// console.log(school.toLowerCase());
+
+// console.log(college);
+// console.log(college.toUpperCase());
+
+// const subject = "Javascript";
+// const sub2 = "javaScript";
+
+// const drink = " water";
+// const liquid = "water  ";
+
+// if (drink.trim() === liquid.trim()) {
+//   console.log("ami ektu pore ghurte jamu");
+// } else {
+//   console.log("ami ektu pore ghurte jabo naaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+// }
+
+// const address = "andorkilla";
+// const part = address.slice(2, 9); // start index, end index
+// // console.log(part);
+
+// const sentence =
+//   "I am a student of, programming hero. I love, programming hero.";
+// console.log(sentence.split("a"));
+
+// const realFrnd = ["Rafiq", "Rifat", "Raihan", "Raihan", "Raihan", "Raihan"];
+// console.log(realFrnd.join('*'))
+
+// const first = "abid";
+// const last = "hasan";
+// const fullName = first.concat(" ").concat(last);
+// // console.log(fullName);
+// console.log(first.includes('x'))
+
+const sentence = "i am learning javascript";
+// const sentence = "donuld trump";
+// let reverse = " ";
+// for (const letter of sentence) {
+//   // console.log(letter);
+//   reverse = letter + reverse;
+// }
+// // console.log(reverse);
+// let rev = " ";
+// for (let i = 0; i < sentence.length; i++) {
+//   //   console.log(sentence[i]);
+//   const ltr = sentence[i];
+//   rev = ltr + rev;
+// }
+// console.log(rev);
+// let reverse = sentence.split("").reverse().join("");
+// let reverse = sentence.reverse();
+
+// console.log(reverse);
+
+// object 
+
+// const age = 21;
+// const name = 'shamim';
+// const isPassed = true;
+// let idDeveloper;
+// const person = '';
+
+// console.log(person)
+
+const person = {
+    name : 'shamim',
+    age: 28,
+    profession: 'police',
+    salary: 25000,
+    isMarried: true
+}
+
+console.log(person);
