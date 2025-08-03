5db8dff8f8c225fccd74b4e35ec71e71660a3d79
Shamim-Islam <shamimislam844gm@gmail.com>
2025-08-03 11:26:22 +0600

    code updated

--- M-15_Js_loops_tasks/for/2 odd even.js
+++ M-15_Js_loops_tasks/for/2 odd even.js
@@ -6,6 +6,11 @@ Find all the odd numbers from 61 to 100.
 
  */
 
+for (let i = 61; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log("odd numbers: " + i);
+  }
+}
 
 /***
 
@@ -16,3 +21,9 @@ Find all the even numbers from 78 to 98.
  */
 
 /*programming hero*/
+
+for (let i = 78; i <= 98; i++) {
+  if (i % 2 === 0) {
+    console.log("even numbers: " + i);
+  }
+}
--- M-15_Js_loops_tasks/for/3 sum of odd numbers.js
+++ M-15_Js_loops_tasks/for/3 sum of odd numbers.js
@@ -1,5 +1,3 @@
-
-
 /***
 
 Subtask-1:
@@ -7,6 +5,16 @@ Subtask-1:
 Display sum of all the odd numbers from 91 to 129.
 
  */
+let sum = 0;
+
+for (let i = 91; i <= 129; i++) {
+  if (i % 2 !== 0) {
+    // console.log(i);
+    sum += i;
+  }
+}
+console.log("Sum of odd numbers from 91 to 129 is : " + sum);
+
 /***
 
 Subtask-2:
@@ -16,3 +24,14 @@ Display sum of all the even numbers from 51 to 85.
  */
 
 /*programming hero*/
+
+let sumEven = 0;
+
+for (let i = 51; i <= 85; i++) {
+  if (i % 2 === 0) {
+    // console.log(i);
+    sumEven += i;
+  }
+}
+
+console.log("Sum of even numbers from 51 to 85 is : " + sumEven);
