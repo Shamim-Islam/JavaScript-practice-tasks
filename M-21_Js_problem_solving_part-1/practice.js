8b207a022a62ee6343cbf83adc3312b15a1aea93
Shamim-Islam <shamimislam844gm@gmail.com>
2025-08-12 00:58:20 +0600

    code updated

---
+++ M-21_Js_problem_solving_part-1/practice.js
@@ -0,0 +1,30 @@
+// findout Min number
+
+const min = Math.min(45, 21, 65, 99, 126, 2, 5, -99);
+console.log(min);
+
+// Find out Max Number
+
+const max = Math.max(45, 21, 65, 99, 126, 2, 5, -99);
+console.log(max);
+
+// PI - 3.14
+
+console.log(Math.PI);
+
+// abs
+console.log(Math.abs(5 - 10));
+
+// Round
+console.log(Math.round(4.4));
+
+// floor
+console.log(Math.floor(3.1));
+
+// ceil
+console.log(Math.ceil(3.99));
+
+// random
+console.log(Math.random() * 10);
+const rand = Math.round(Math.random() * 10);
+console.log(rand);
