e82c23357fb11df2524c7f19227282503d72c554
Shamim-Islam <shamimislam844gm@gmail.com>
2025-08-20 00:11:54 +0600

    added Module 23 & 24

---
+++ M-18_All_about_Array/array_looping_task-1.js
@@ -0,0 +1,9 @@
+// Array looping task - 1
+
+// Write a JavaScript code to reverse the array colors without using the reverse method
+
+const colors = ["red", "blue", "green", "yellow", "orange"];
+
+for (let i = colors; i > 0; i--) {
+  console.log(i);
+}
